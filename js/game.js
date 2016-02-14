2bf1f97a12648980ef849bb2d03d101cec329059
Robert Sigler <sigle023@umn.edu>
2016-02-13 18:00:55 -0600

    add sprite assets

--- js/game.js
+++ js/game.js
@@ -1,21 +1,22 @@
 var Q = Quintus()
-        .include("Sprites, Scenes, Input, 2D, Touch, UI")
+        .include("Sprites, Scenes, Input, Touch")
         .setup({ maximize: true })
-        .controls().touch()
+        .controls().touch();
 
-Q.Sprite.extend("Player",{
-  init: function(p) {
-    this._super(p, { sheet: "player", x: 64, y: 64 });
-  }
-});
+Q.Sprite.extend("Background" {
+    init: function(p) {
+        this._super(p, {
+            asset: "bg.png"
+            x: 0,
+            y: 0
+        });
+    }
+})
 
 Q.scene("level1",function(stage) {
-  stage.collisionLayer(new Q.TileLayer({ dataAsset: 'level.json', sheet: 'tiles' }));
-  var player = stage.insert(new Q.Player());
+    var bg = stage.insert(new Q.Background());
 });
 
-Q.load("sprites.png, sprites.json, level.json, tiles.png", function() {
-  Q.sheet("tiles","tiles.png", { tilew: 32, tileh: 32 });
-  Q.compileSheets("sprites.png","sprites.json");
-  Q.stageScene("level1");
+Q.load("bg.png, obstalce.png, player.png", function() {
+    Q.stageScene("level1");
 });
