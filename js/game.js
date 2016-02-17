cad6fa714514680cba0394d8e09b7246b7e49395
Robert Sigler <sigle023@umn.edu>
2016-02-16 23:14:01 -0600

    testing pixel art scaling

--- js/game.js
+++ js/game.js
@@ -45,13 +45,26 @@ Q.MovingSprite.extend("Player", {
         p.x += p.vx * dt;
     }
     */
-})
+});
+
+Q.Sprite.extend("Catty", {
+    init: function(p) {
+        this._super(p, {
+            x: 100,
+            y: 100,
+            w: 105,
+            h: 92,
+            asset: 'cattytest.png'
+        });
+    }
+});
 
 Q.scene("level1",function(stage) {
     var battlebox = stage.insert(new Q.Battlebox());
     var player = stage.insert(new Q.Player({ vx : 10 }));
+    var cattytest = stage.insert(new Q.Catty());
 });
 
-Q.load("bg.png, obstacle.png, player.png", function() {
+Q.load("bg.png, obstacle.png, player.png, cattytest.png", function() {
     Q.stageScene("level1");
 });
