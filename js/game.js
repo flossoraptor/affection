49a4ad7ba9a9967fdbac2f1f4b3a3c3453703865
Robert Sigler <sigle023@umn.edu>
2016-04-21 23:39:18 -0500

    add taking damage and temporary invulernability

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ var quintusOptions = {
 };
 
 var Q = Quintus(quintusOptions)
-        .include("Sprites, Scenes, Input, Pixels");
+        .include("Sprites, Scenes, Input, Pixels, UI");
 
 var setupOptions = {
     width: 400,
@@ -22,7 +22,25 @@ Q.MovingSprite.extend("Player", {
             h: 16,
             asset: 'player.png'
         });
+
+        this.p.invincibility = {timer:0};
+        this.p.flicker = {count:0};
         this.p.speed = 100;
+        this.p.hp = 20;
+        if (p) {
+            this.p.hpText = p.hpText;
+            this.p.hpText.p.label = '' + this.p.hp;
+        }
+
+        this.on("hit.sprite",function(collision) {
+            if (collision.obj.isA("Bullet")) {
+                if (this.p.invincibility.timer == 0) {
+                    this.p.hp = this.p.hp - 1;
+                    this.updateHp(this.p.hp);
+                    this.triggerInvincibility(2);
+                }
+            }
+        });
     },
 
     step: function(dt) {
@@ -42,7 +60,56 @@ Q.MovingSprite.extend("Player", {
         if (Q.inputs['down']) {
             p.vy += p.speed;
         }
+
+        this.step_invincibility(dt);
+
+        this.stage.collide(this);
+
         this._super(dt);
+    },
+
+    step_invincibility: function(dt) {
+        var p = this.p;
+        if (p.invincibility.timer > 0) {
+            p.invincibility.timer = p.invincibility.timer - dt;
+            if (p.invincibility.timer < 0) {
+                this.endInvincibility()
+            } else {
+                // Should cause the sprite to flicker
+                this.step_flicker(dt);
+            }
+        }
+    },
+
+    step_flicker: function(dt) {
+        var p = this.p;
+        if (p.flicker.count == 0) {
+            this.toggle_opacity();
+            p.flicker.count = 2;
+        } else {
+            p.flicker.count = p.flicker.count - 1;
+        }
+    },
+
+    toggle_opacity: function() {
+        if (this.p.opacity != 0) {
+            this.p.opacity = 0;
+        } else {
+            this.p.opacity = 0.75;
+        }
+    },
+
+    triggerInvincibility: function(seconds) {
+        this.p.invincibility = {timer:seconds};
+        this.p.opacity = 1;
+    },
+
+    endInvincibility: function() {
+        this.p.invincibility = {timer:0};
+    },
+
+    updateHp: function(hp) {
+        this.p.hpText.p.label = '' + hp;
     }
 });
 
@@ -63,27 +130,27 @@ Q.MovingSprite.extend("Bullet", {
         } else if (p.y < 0 || p.y > 280) {
             this.destroy();
         }
+        this.stage.collide(this);
     }
 })
 
 Q.MovingSprite.extend("BulletShooter", {
-    init: function(p, stage) {
+    init: function(p) {
         this._super(p, {
             w: 8,
             h: 8,
             asset: 'bullet.png'
         });
-        this.stage = stage;
-        this.timer = 0;
+        this.p.timer = 0;
     },
 
     step: function(dt) {
         var p = this.p;
-        this.timer += dt;
-        if (this.timer > 1) {
-            this.timer = 0;
+        p.timer += dt;
+        if (p.timer > 1) {
+            p.timer = 0;
         }
-        if (this.timer == 0) {
+        if (p.timer == 0) {
             this.stage.insert(new Q.Bullet({x:p.x, y:p.y, vx:80}));
             this.stage.insert(new Q.Bullet({x:p.x, y:p.y, vx:-80}));
             this.stage.insert(new Q.Bullet({x:p.x, y:p.y, vy:80}));
@@ -105,8 +172,22 @@ Q.MovingSprite.extend("BulletShooter", {
 })
 
 Q.scene("level1",function(stage) {
-    //var battlebox = stage.insert(new Q.Battlebox());
-    var player = stage.insert(new Q.Player());
+    var container = stage.insert(new Q.UI.Container({
+        fill: "black",
+        border: 5,
+        shadow: 10,
+        shadowColor: "rgba(0,0,0,0.5)",
+        x: 35,
+        y: 0
+    }));
+    var hpText = new Q.UI.Text({
+        label: "40",
+        color: "white",
+        x: 0,
+        y: 0
+    });
+    stage.insert(hpText, container);
+    var player = stage.insert(new Q.Player({hpText:hpText}));
     stage.insert(new Q.BulletShooter({x:75, y:75, vx:30}, stage));
     stage.insert(new Q.BulletShooter({x:150, y:125, vx: 30, vy: 15}, stage));
     stage.insert(new Q.BulletShooter({x:225, y:175, vx: 15, vy: -30}, stage));
